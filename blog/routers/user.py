161cf70155a995c6ef8524546eb8448bbffbe495
JaysanJaychiev <jays.26@mail.ru>
2021-10-18 09:40:52 +0600

    add routers

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -9,7 +9,7 @@ get_db = database.get_db
 
 
 
-@app.post('/user', response_model=schemas.ShowUser, tags=['users'])
+@router.post('/user', response_model=schemas.ShowUser, tags=['users'])
 def create_user(request: schemas.User, db : Session = Depends(get_db)):
     
     new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
@@ -19,7 +19,7 @@ def create_user(request: schemas.User, db : Session = Depends(get_db)):
     return new_user
 
 
-@app.get('/user/{id}', response_model=schemas.ShowUser, tags=['users'])
+@router.get('/user/{id}', response_model=schemas.ShowUser, tags=['users'])
 def get_user(id:int, db : Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
